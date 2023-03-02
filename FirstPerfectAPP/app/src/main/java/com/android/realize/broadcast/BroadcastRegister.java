cb330cc61057abda07ee252b4acbc78b76074cfe
zenghui.chen <zenghui.chen@thundersoft.com>
2023-03-02 15:22:43 +0800

    delete test

--- FirstPerfectAPP/app/src/main/java/com/android/realize/broadcast/BroadcastRegister.java
+++ FirstPerfectAPP/app/src/main/java/com/android/realize/broadcast/BroadcastRegister.java
@@ -13,7 +13,6 @@ public class BroadcastRegister extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         UiLog.i(TAG, "动态注册广播");
-        UiLog.i(TAG, "12314动态注册广播");
         Toast.makeText(context, "动态注册广播", Toast.LENGTH_SHORT).show();
         // 如果广播是顺序广播，到该广播时截断广播，后续广播不在接收广播
         abortBroadcast();
