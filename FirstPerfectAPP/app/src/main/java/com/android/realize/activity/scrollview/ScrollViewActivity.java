1fbb79232f3fa1a513451d00e05fdbb772d22d97
zenghui.chen <zenghui.chen@thundersoft.com>
2023-05-06 14:27:44 +0800

    scrollViewActivity

--- FirstPerfectAPP/app/src/main/java/com/android/realize/activity/scrollview/ScrollViewActivity.java
+++ FirstPerfectAPP/app/src/main/java/com/android/realize/activity/scrollview/ScrollViewActivity.java
@@ -1,14 +1,23 @@
 package com.android.realize.activity.scrollview;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ScrollView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.realize.R;
 import com.android.realize.interfacces.IGeneralInterface;
+import com.android.realize.unicom.UiLog;
 
 public class ScrollViewActivity extends AppCompatActivity implements IGeneralInterface {
     private static final String TAG = "ScrollViewActivity";
+    private OnClick mOnClick;
+    private ScrollView mScrollView;
+    private Button mScrollViewUp;
+    private Button mScrollViewDown;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,11 +43,40 @@ public class ScrollViewActivity extends AppCompatActivity implements IGeneralInt
 
     @Override
     public void init() {
+        mOnClick = new OnClick();
 
+        mScrollViewUp = findViewById(R.id.activity_scrollview_btn_up);
+        mScrollViewDown = findViewById(R.id.activity_scrollview_btn_down);
+        mScrollView = findViewById(R.id.activity_scrollview_sv);
+        mScrollViewUp.setOnClickListener(mOnClick);
+        mScrollViewDown.setOnClickListener(mOnClick);
     }
 
     @Override
     public void release() {
 
+    }
+
+    private class OnClick implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                // 移动到滚动框顶端
+                case R.id.activity_scrollview_btn_up: {
+                    mScrollView.fullScroll(View.FOCUS_UP);
+                    break;
+                }
+                // 移动到滚动框末端
+                case R.id.activity_scrollview_btn_down: {
+                    mScrollView.fullScroll(View.FOCUS_DOWN);
+                    break;
+                }
+                default: {
+                    UiLog.w(TAG, "event error");
+                    break;
+                }
+            }
+        }
     }
 }
